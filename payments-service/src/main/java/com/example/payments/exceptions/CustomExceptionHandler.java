d226ec77adf65f9fac31ad27eca6ef5e15981a89
gitcrumbs <ashwanisingh034@gmail.com>
2022-11-25 11:54:44 +0530

    Implemented Exception Handlers

--- payments-service/src/main/java/com/example/payments/exceptions/CustomExceptionHandler.java
+++ payments-service/src/main/java/com/example/payments/exceptions/CustomExceptionHandler.java
@@ -1,15 +1,16 @@
 package com.example.payments.exceptions;
 
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
@@ -25,4 +26,20 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity(errorDetails,HttpStatus.BAD_REQUEST);
 
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+       MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request){
+
+        Map<String,Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("message","Invalid mode of payment");
+        responseBody.put("statusCode",status.value());
+
+
+        return new ResponseEntity<>(responseBody,headers,status);
+
+
+    }
+
+
 }
